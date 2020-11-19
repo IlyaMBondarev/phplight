427ae842ece01ea0f972feaff869561137be917e
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2020-11-19 18:55:00 +0300

    Add files via upload

--- templates/pages/gallery_item.php
+++ templates/pages/gallery_item.php
@@ -1,5 +1,13 @@
 <a href="/gallery">Вернуться</a>
 <div style="height: 50vw; width: 50vw;">
-<img src="<?= '/' . $src ?>" alt="photo" style="width: 100%; height: 100%;">
+<img src="<?= '/' . $item['src'] ?>" alt="photo" style="width: 100%; height: 100%;">
 </div>
-<p>Количество просмотров: <?= $views ?></p>
+<p>Количество просмотров: <?= $item['views'] ?></p>
+<div>
+    <?php foreach ($itemReplies as $reply): ?>
+    <div style="padding: 20px; border-top: 1px solid grey;">
+        <p style="font-weight: bold; font-size: 30px"><?= $reply['name'] ?></p>
+        <p><?= $reply['content'] ?></p>
+    </div>
+    <?php endforeach; ?>
+</div>
